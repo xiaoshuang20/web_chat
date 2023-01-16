3c2f3827692778410f51643cfd57afb4ebebf826
xiaoshuang <2238929232@qq.com>
2023-01-16 22:34:45 +0800

    fix

--- src/components/ChatApp.vue
+++ src/components/ChatApp.vue
@@ -50,7 +50,7 @@
             </div>
           </li>
         </ul>
-        <div class="footer">进阶功能（我的/气泡样式）</div>
+        <div class="footer">进阶功能（我的/气泡样式/历史记录/创建房间）</div>
       </div>
       <BackgroundPanel />
     </div>
